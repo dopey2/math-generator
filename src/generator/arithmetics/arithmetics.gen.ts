976d086dd8c96b20b634a5569f0c52daf35166c9
DESKTOP-3LQOJ00\Dani <dani.gabor1996@gmail.com>
2022-05-01 10:16:22 +0200

    refactor(generator): use math-x-ts to solve fractions

--- src/generator/arithmetics/arithmetics.gen.ts
+++ src/generator/arithmetics/arithmetics.gen.ts
@@ -1,31 +1,6 @@
 import ExerciseBuilder from "../ExerciseBuilder";
 import MathX from "../../math/MathX/MathX";
-import { evaluate } from "@math-x-ts/compiler";
-
-
-// TODO REMOVE THIS
-type Fraction = {
-    dividend: number;
-    divisor: number;
-    quotient?: number;
-}
-
-// TODO REMOVE THIS
-type BaseOperator = '+' | '-' | '*' | ':';
-
-// TODO REMOVE THIS
-const baseOperation = (term1: number, term2: number, operator: BaseOperator) => {
-    switch (operator) {
-    case "+":
-        return term1 + term2;
-    case "-":
-        return term1 - term2;
-    case "*":
-        return term1 * term2;
-    case ":":
-        return term1 / term2;
-    }
-};
+import { evaluate, parse } from "@math-x-ts/parser";
 
 export const addition = (
     terms: number,
@@ -56,7 +31,6 @@ export const multiplication = (args: {
         .addAnswerLatex(`${expression} = ${value}`)
         .toJSON();
 };
-;
 
 export const division = (args: {
     minNumber?: number,
@@ -83,168 +57,29 @@ export const radical = (maxNumber: number) => {
         .toJSON();
 };
 
-
-type FractionOperationWithStep = {
-    dividend: number;
-    divisor: number;
-    latexStep: string[];
-}
-
-const solveFractionOperation = (
-    fractionA: Fraction,
-    fractionB: Fraction,
-    operator: BaseOperator
-): FractionOperationWithStep => {
-    if (operator === '*' || operator === ':') {
-        let _fractionB = { ...fractionB };
-        if (operator === ':') {
-            _fractionB = {
-                dividend: fractionB.divisor,
-                divisor: fractionB.dividend,
-            };
-        }
-
-        const step1_dividend = `${fractionA.dividend} * ${_fractionB.dividend}`;
-        const step1_divisor = `${fractionA.divisor} * ${_fractionB.divisor}`;
-        const dividend = fractionA.dividend * _fractionB.dividend;
-        const divisor = fractionA.divisor * _fractionB.divisor;
-
-        return {
-            dividend,
-            divisor,
-            latexStep: [
-                `\\frac{${step1_dividend}}{${step1_divisor}}`,
-                `\\frac{${dividend}}{${divisor}}`
-            ],
-        };
-    } else {
-        if (fractionA.divisor === fractionB.divisor) {
-            const dividend = baseOperation(fractionA.dividend, fractionB.dividend, operator);
-
-            return {
-                dividend: dividend,
-                divisor: fractionA.divisor,
-                latexStep: [
-                    `\\frac{${fractionA.dividend} ${operator} ${fractionB.dividend}}{${fractionA.divisor}}`,
-                    `\\frac{${dividend}}{${fractionA.divisor}}`
-                ],
-            };
-        } else {
-            let inverse = fractionA.divisor < fractionB.divisor;
-            let _fractionA = { ...(inverse ? fractionB : fractionA) };
-            let _fractionB = { ...(inverse ? fractionA : fractionB) };
-            const divisionRes = _fractionA.divisor / _fractionB.divisor;
-
-            const steps = [];
-
-            let step1_FractionAResult = { ..._fractionA };
-            let step1_FractionBResult = { ..._fractionB };
-
-            /**
-             * Transform to same divisor
-             */
-            if ((divisionRes) - Math.floor(divisionRes) === 0) {
-                /**
-                 * Case multiply by constant
-                 */
-                const step1_fractionB_dividend = `${_fractionB.dividend} * ${divisionRes}`;
-                const step1_fractionB_divisor = `${_fractionB.divisor} * ${divisionRes}`;
-
-                const arr = [
-                    `\\frac{${_fractionA.dividend}}{${_fractionA.divisor}}`,
-                    `\\frac{${step1_fractionB_dividend}}{${step1_fractionB_divisor}}`
-                ];
-
-                let leftIndex = inverse ? 1 : 0;
-                let rightIndex = inverse ? 0 : 1;
-
-                steps.push(`${arr[leftIndex]} ${operator} ${arr[rightIndex]}`);
-
-                step1_FractionBResult.dividend = _fractionB.dividend * divisionRes;
-                step1_FractionBResult.divisor = _fractionB.divisor * divisionRes;
-            } else {
-                /**
-                 * multiply by other fraction divisor
-                 */
-                const step1_fractionA_dividend = `${_fractionA.dividend} * ${_fractionB.divisor}`;
-                const step1_fractionA_divisor = `${_fractionA.divisor} * ${_fractionB.divisor}`;
-
-                const step1_fractionB_dividend = `${_fractionB.dividend} * ${_fractionA.divisor}`;
-                const step1_fractionB_divisor = `${_fractionB.divisor} * ${_fractionA.divisor}`;
-
-                steps.push(`
-                \\frac{${step1_fractionA_dividend}}{${step1_fractionA_divisor}} 
-                ${operator}
-                \\frac{${step1_fractionB_dividend}}{${step1_fractionB_divisor}}    
-                `);
-
-                step1_FractionAResult.dividend = _fractionA.dividend * _fractionB.divisor;
-                step1_FractionAResult.divisor = _fractionA.divisor * _fractionB.divisor;
-                step1_FractionBResult.dividend = _fractionB.dividend * _fractionA.divisor;
-                step1_FractionBResult.divisor = _fractionB.divisor * _fractionA.divisor;
-            }
-
-            const finalFractionA = { ...(inverse ? step1_FractionBResult : step1_FractionAResult) };
-            const finalFractionB = { ...(inverse ? step1_FractionAResult : step1_FractionBResult) };
-
-            let step2_fractionResult = solveFractionOperation(finalFractionA, finalFractionB, operator);
-            step2_fractionResult.latexStep = [
-                ...steps,
-                ...step2_fractionResult.latexStep
-            ];
-            return step2_fractionResult;
-        }
-    }
-};
-
-
-// TODO use math/Operation/Fraction
 export const fractionRulesExpression = () => {
-    const fractionA = {
-        dividend: MathX.random(1, 10),
-        divisor: MathX.random(1, 10),
-    };
 
-    const fractionB = {
-        dividend: MathX.random(1, 10),
-        divisor: MathX.random(1, 10),
-    };
+    const fracA_num = MathX.random(1, 10);
+    const fracA_den = MathX.random(1, 10);
 
-    const fractionC = {
-        dividend: MathX.random(1, 10),
-        divisor: MathX.random(1, 10),
-    };
+    const fracB_num = MathX.random(1, 10);
+    const fracB_den = MathX.random(1, 10);
 
-    // const expression1 = `${fractionA.dividend}/${fractionA.divisor}`;
-    // const expression2 = `${fractionB.dividend}/${fractionB.divisor}`;
-    // const expression3 = `${fractionC.dividend}/${fractionC.divisor}`;
+    const fracC_num = MathX.random(1, 10);
+    const fracC_den = MathX.random(1, 10);
 
     const operator1 = MathX.random(0, 1) ? '*' : ':';
     const operator2 = MathX.random(0, 1) ? '+' : '-';
 
-    const fractionAB = solveFractionOperation(fractionA, fractionB, operator1);
-    const fractionABC = solveFractionOperation(fractionAB, fractionC, operator2);
+    const expression = `{${fracA_num}} / {${fracA_den}} ${operator1} {${fracB_num}} / {${fracB_den}} ${operator2} {${fracC_num}} / {${fracC_den}}`;
+    const mathNode = parse(expression);
 
-    // const finalExpression = `${expression1} ${operator1} ${expression2} ${operator2} ${expression3}`;
-    // const value = 0;
-
-    const steps = [];
-
-    const latex = `\\frac{${fractionA.dividend}}{${fractionA.divisor}} ${operator1} \\frac{${fractionB.dividend}}{${fractionB.divisor}}${operator2} \\frac{${fractionC.dividend}}{${fractionC.divisor}}`;
-
-    steps.push(latex);
-
-    fractionAB.latexStep.forEach((step) => {
-        steps.push(`${step} ${operator2} \\frac{${fractionC.dividend}}{${fractionC.divisor}}`);
-    });
-
-    fractionABC.latexStep.forEach((step) => {
-        steps.push(step);
-    });
+    const response = mathNode.solve().toTex();
+    const steps = mathNode.solveAllToTex();
 
     return new ExerciseBuilder()
-        .addQuestionLatex(latex)
-        .addAnswerLatex(`${steps[steps.length - 1]}`)
+        .addQuestionLatex(mathNode.toTex())
+        .addAnswerLatex(response)
         .addStepAnswerLatex(...steps)
         .toJSON();
 };
